ae15f3b57a6a71e49b85f4aa2d0fc132ef1cfe70
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-06-14 06:41:17 -0500

    Add files via upload
    about to set up routes

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
+# 9.5.1 add dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+# import SQLAlchemy dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+# import flask dependencies
+from flask import Flask, jsonify
+
+# set up db engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect db into classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# save our references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create our seesion link
+session = Session(engine)
+
+# define our flask app
+app = Flask(__name__)
+
+# create our flask routs
+@app.route("/")
+def welcome():
+    return
+
+
+
+
 # 9.4.1, 9.4.2 and 9.4.3 - flask app
 from flask import Flask
 # create an instance
