1b7c9c5db6a0d02f853de2389736d354ee0c144d
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-06-14 06:52:07 -0500

    Add files via upload
    lines 44-49 about to jsonify the precip data

--- app.py
+++ app.py
@@ -27,22 +27,35 @@ session = Session(engine)
 # define our flask app
 app = Flask(__name__)
 
-# create our flask routs
+# 9.5.2 - create our flask routs
 @app.route("/")
 def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+# 9.5.3 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
     return
 
-
-
-
-# 9.4.1, 9.4.2 and 9.4.3 - flask app
-from flask import Flask
-# create an instance
-app = Flask(__name__)
-# create route - define a starting point
-@app.route('/')
-# create a function for the root
-def hello_world():
-    return 'Hello World'
-# navigate to folder in terminal and run: export FLASK_APP=app.py
-# followed by: flask run
+#### 9.4.1, 9.4.2 and 9.4.3 - flask app
+# from flask import Flask
+#### create an instance
+# app = Flask(__name__)
+#### create route - define a starting point
+# @app.route('/')
+#### create a function for the root
+# def hello_world():
+#     return 'Hello World'
+#### navigate to folder in terminal and run: export FLASK_APP=app.py
+#### followed by: flask run
