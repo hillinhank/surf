2ce8426618eea015347a45f9dca987420feed6d4
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-06-14 06:27:04 -0500

    Add files via upload
    hello world is running!

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 # 9.4.1, 9.4.2 and 9.4.3 - flask app
+from flask import Flask
+# create an instance
+app = Flask(__name__)
+# create route - define a starting point
+@app.route('/')
+# create a function for the root
+def hello_world():
+    return 'Hello World'
+# navigate to folder in terminal and run: export FLASK_APP=app.py
+# followed by: flask run
